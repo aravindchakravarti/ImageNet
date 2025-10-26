7a2190dd38509c6d494c22868ab7ab84dd5c57ff
Aravind D. Chakravarti <aravinddcsadguru@gmail.com>
2025-10-26 18:14:02 +0530

    Shifting to ResNet-50

--- model_v2.py
+++ model_v2.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from config import Config
 
 def get_model(weights=None):
-    model = models.resnet18(weights=weights)
+    model = models.resnet50(weights=weights)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, Config.NUM_CLASSES)
     return model
